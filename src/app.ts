275d97b948714907c8a39d017db70d28284a07f3
Vincent Gonnet <vincent.gonnet08@gmail.com>
2025-03-04 17:40:46 +0100

    fix: wait for db connection to start the server

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,42 @@ app.get("/", (req, res) => {
     res.send("Hello World ! Here is your Express.js backend")
 });
 
-// Synchronize models with database
-db.sequelize.sync().then(() => {
-    console.log('Database synchronized');
-    // http://localhost:3000/
-    app.listen(3000, () => {
-        console.log('Server is up and listening on port 3000');
-    });
-});
+async function authenticateDatabase(retries = 5, delay = 3000) {
+    while (retries > 0) {
+        try {
+            await db.sequelize.authenticate();
+            console.log('Connection has been established successfully.');
+            break;
+        } catch (error) {
+            console.error('Unable to connect to the database');
+            retries -= 1;
+            console.log(`Retries left: ${retries}`);
+            if (retries === 0) {
+                console.error('All retries exhausted. Exiting.');
+                process.exit(1);
+            }
+            await new Promise(res => setTimeout(res, delay));
+        }
+    }
+}
+
+async function startServer() {
+    try {
+        // Wait for database connection
+        await authenticateDatabase();
+
+        // Synchronize models with database
+        await db.sequelize.sync();
+        console.log('Database synchronized');
+
+        // Start the server
+        app.listen(3000, () => {
+            console.log('Server is up and listening on port 3000');
+        });
+        
+    } catch (error) {
+        console.error('Unable to start the server :', error);
+    }
+}
+
+startServer();
--- src/config/database.ts
+++ src/config/database.ts
@@ -6,15 +6,4 @@ const db: { [key: string]: any } = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-async function authenticateDatabase() {
-    try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-}
-
-authenticateDatabase();
-
 export default sequelize;
