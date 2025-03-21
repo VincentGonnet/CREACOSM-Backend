fa86a52b6777d4cbfd5e6626b9b2b6b86ab6c3ba
VincentGonnet <vincent.gonnet08@gmail.com>
2025-03-21 13:49:57 +0100

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import cors from "cors";
 import startServer from "./init";
 
 import gameRouter from "./routes/game.route";
@@ -8,6 +9,7 @@ import storageRouter from "./routes/storages.route";
 
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", gameRouter);
 app.use("/", discoveriesRouter);
